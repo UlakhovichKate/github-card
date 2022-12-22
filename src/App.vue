8cd0a86b3fa887e3077c87f87ac2238515b718e0
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2022-12-22 14:22:52 +0300

    result

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="cards">
+    <ProfileCard v-for="item in people"
+                 :key=item.id
+                 :info=item.url
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProfileCard from './components/ProfileCard.vue'
+import axios from 'axios'
+import Token from '../delivery-methods'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProfileCard
+  },
+  data() {
+    return {
+      people: null,
+    }
+  },
+  created () {
+    axios
+        .get('https://api.github.com/users', {
+          headers: {
+            Authorization: 'Bearer ' + Token.token,
+          }
+        })
+        .then(response => (this.people = response.data))
   }
 }
 </script>
@@ -22,5 +42,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 }
 </style>
